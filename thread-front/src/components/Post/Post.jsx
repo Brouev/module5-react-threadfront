d25cb296ea704579b0895b0a2201fa9609b3ed41
PaulOcho26 <paul.bourlier2026@gmail.com>
2025-12-09 15:01:19 +0100

    premier commit Post jsx & css

---
+++ thread-front/src/components/Post/Post.jsx
@@ -0,0 +1,44 @@
+
+
+// export default function Post({ post }) {
+//   return (
+//     <div className="post">
+//       <h3>@Ryu-du57</h3>
+//       <p>Aujourd'hui je me suis promené sous la pluie.</p>
+//       <Link to={`/posts/${post.id}`}>Voir le post</Link>
+//     </div>
+//   );
+// }
+import {Link} from 'react-router-dom';
+import PostDetailCard from '../PostDetailCard/PostDetailCard.jsx';
+import CommentCard from '../CommentCard/CommentCard.jsx';
+import './Post.css';
+
+function Post({post}) {
+  return (
+    <div className="post-container">
+      <div className="post-detail">
+        <h1>Post</h1>
+        <PostDetailCard post={post} />
+      </div>
+      <div className="post-comments">
+        <p>{post.comments.length} icone de commentaire</p>
+        {post.comments.map (comment => <CommentCard comment={comment} key={comment.id} />)}
+      </div>
+      <div className="nav-buttons">
+        <Link to={'/add-post'}>
+          {/* Ajouter l'icone correspondante */}
+        </Link>
+        <Link to={'/profile'}>
+          {/* Ajouter l'icone correspondante */}
+        </Link>
+        <Link to={'/feed'}>
+          {/* Ajouter l'icone correspondante */}
+        </Link>
+      </div>
+
+    </div>
+  );
+}
+
+export default Post;
