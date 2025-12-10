ea54cc820f398f34b6acccb30337c3d58f0addd9
Billel Boualam <boualambillelsofien@gmail.com>
2025-12-10 14:30:46 +0100

    removed useless comments

--- back/Helpers/database.mjs
+++ back/Helpers/database.mjs
@@ -10,15 +10,12 @@ await createTable(sequelize);
  */
 export async function loadSequelize() {
     try {
-        // CORRECTION ICI : On utilise process.env pour lire les variables Docker
         const sequelize = new Sequelize(
-            process.env.DB_NAME,     // Au lieu de 'thread-api' en dur
-            process.env.DB_USER,     // Au lieu de 'root' en dur
-            process.env.DB_PASSWORD, // Au lieu de 'root' en dur
+            process.env.DB_NAME,
+            process.env.DB_USER,     
+            process.env.DB_PASSWORD, 
             {
                 dialect: "mysql",
-                // C'est le point CRUCIAL : cela va lire "mysql" depuis le docker-compose
-                // Le "|| 'localhost'" sert de sécurité si tu lances le script hors de Docker
                 host: process.env.DB_HOST || "localhost" 
             }
         );
@@ -28,8 +25,6 @@ export async function loadSequelize() {
         return sequelize;
     } catch (error) {
         console.error("Erreur Sequelize :", error);
-        // Petit conseil : ne pas throw ici si tu veux gérer les retries, 
-        // mais pour l'instant laissons comme ça.
         throw Error("Échec du chargement de Sequelize");
     }
 }
