2e75569e56515ca8103746266803dc21eef5f98a
Billel Boualam <boualambillelsofien@gmail.com>
2025-12-12 15:21:35 +0100

    fixed wrong syntax on author

--- thread-front/src/components/PostDetailCard/PostDetailCard.jsx
+++ thread-front/src/components/PostDetailCard/PostDetailCard.jsx
@@ -5,7 +5,7 @@ function PostDetailCard({post}) {
     return (
         <div className="post-detail-card">
             <div className="post-detail-info">
-                <h3 className="post-author-name">@{post.author.username}</h3>
+                <h3 className="post-author-name">@{post.author}</h3>
                 <p className="post-content">{post.content}</p>
             </div>
             <div className="post-detail-date">
